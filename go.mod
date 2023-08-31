2ea6437727a9edd24852dbfd71709448e32bf80b
Kentoes PDAM <108379342+kentoespdam@users.noreply.github.com>
2023-09-01 00:39:27 +0700

    hello world

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kentoespdam/app-belajar-module.git
+
+go 1.21.0
+
+require github.com/kentoespdam/belajar-module v1.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	belajar_module "github.com/kentoespdam/belajar-module"
+)
+
+func main() {
+	fmt.Println(belajar_module.SayHello())
+}
